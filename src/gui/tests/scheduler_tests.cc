1d6f8d8d3026b3e7cb559dc5b02b75a9729276d9
houstudio <1225012331@qq.com>
2020-08-18 21:16:26 -0700

    add scheduler testcase

--- src/gui/tests/scheduler_tests.cc
+++ src/gui/tests/scheduler_tests.cc
@@ -13,13 +13,28 @@ class SCHEDULER:public testing::Test{
    virtual void TearDown(){
    }
 };
+struct SubscribeItem{
+   int serviceid;
+   system_clock::time_point time;
+   UINT eventid;
+   UINT duration;/*unint in seconds*/
+   std::string name;
+   void onTriggered(){
+      printf("triggered serviceid:%d event.name=%s\r\n",serviceid,name.c_str());
+   }
+};
 
 
 TEST_F(SCHEDULER,Once){
    Scheduler sch;
    int count=0;
+   SubscribeItem itm;
+   itm.serviceid=100;
+   itm.name="test";
    sch.schedule([&count](){count++;},system_clock::now()+std::chrono::seconds(10));
+   sch.schedule(std::bind(&SubscribeItem::onTriggered,&itm),system_clock::now()+std::chrono::seconds(10));
    sleep(10);
+   sch.check(); 
    sch.check();
    ASSERT_EQ(count,1);
 }
@@ -36,7 +51,11 @@ TEST_F(SCHEDULER,FromNow){
 TEST_F(SCHEDULER,Every){
    Scheduler sch;
    int count=0;
+   SubscribeItem itm;
+   itm.serviceid=200;
+   itm.name="test.every 10 seconds!";
    sch.scheduleEvery([&count](){count++;},5);
+   sch.scheduleEvery(std::bind(&SubscribeItem::onTriggered,&itm),5);
    for(int i=0;i<20;i++){
        sleep(1);
        sch.check();
@@ -79,3 +98,4 @@ TEST_F(SCHEDULER,Weekly){
    sch.check();
    ASSERT_EQ(count,0);
 }
+
